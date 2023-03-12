3335874b844c1ca9ad4e69744024c0678923188d
remishevskaya <heyjude945@gmail.com>
2023-03-12 20:20:20 +0300

    Сделала 6 задание

---
+++ Seminar1/Task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse (Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.Write("Число четное");
+}
+else
+{
+   Console.Write("Число нечетное"); 
+}

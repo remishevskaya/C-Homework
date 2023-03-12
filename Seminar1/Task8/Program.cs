ca5ebbc7d5bccd5a0622590b9e9a741d36c93b23
remishevskaya <heyjude945@gmail.com>
2023-03-12 20:27:02 +0300

    Сделала 8 задание

---
+++ Seminar1/Task8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse (Console.ReadLine());
+int count = 1;
+
+while (count < num + 1)
+{
+    if (count % 2 == 0)
+    {
+        Console.WriteLine(count);
+    }
+    count = count + 1;
+}

c66e2eb47822faa287d37d0b229598d93e1846b9
remishevskaya <heyjude945@gmail.com>
2023-03-12 20:42:53 +0300

    Сделала 7 задание

---
+++ Seminar1/Task7/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse (Console.ReadLine());
+
+int num_left = num / 10 % 10;
+
+if (num_left == 0)
+{
+    Console.Write("Нет");
+}
+else
+{
+    Console.Write(num_left);
+}

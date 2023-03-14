15a7bcca03b50ae4897683fa1487d795f3df5b02
remishevskaya <heyjude945@gmail.com>
2023-03-14 22:33:47 +0300

    Сделала задание 15

---
+++ Seminar2/Task15/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите номер дня недели: ");
+int day = int.Parse (Console.ReadLine());
+
+switch (day)
+{
+    case 1:
+        Console.WriteLine("Нет");
+        break;
+    case 2:
+        Console.WriteLine("Нет");
+        break;
+    case 3:
+        Console.WriteLine("Нет");
+        break;
+    case 4:
+        Console.WriteLine("Нет");
+        break;
+    case 5:
+        Console.WriteLine("Нет");
+        break;
+    case 6:
+        Console.WriteLine("Да");
+        break;
+    case 7:
+        Console.WriteLine("Да");
+        break;
+    default:
+        Console.WriteLine("Дня недели с таким номером не существует");
+        break;
+}

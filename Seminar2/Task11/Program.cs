18e605908493f5f05757d65bd64caa05f8033f56
remishevskaya <heyjude945@gmail.com>
2023-03-15 09:57:47 +0300

    Сделала задание 11

---
+++ Seminar2/Task11/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse (Console.ReadLine());
+int num2 = num;
+int count = 1;
+int mult = 1;
+int num3 = 0;
+
+while (num2 / 10 != 0)
+{
+    num2 = num2 / 10;
+    count++;
+}
+
+while (count > 0)
+{
+    if (count != 2)
+    {
+        num3 = num3 + num % 10 * mult;
+        mult = mult * 10;
+    }
+    num = num / 10;
+    count = count - 1;
+}
+
+Console.WriteLine(num3);

14a39c6907ceb7100e5083ab6a52a2dcd252a677
remishevskaya <heyjude945@gmail.com>
2023-03-16 23:42:00 +0300

    Сделала задание 21

---
+++ Seminar3/Task21/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите координату X первой точки: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Y первой точки: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Z первой точки: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату X второй точки: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Y второй точки: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Z второй точки: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double KoordDist(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    double dist = 0;
+
+    dist = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
+
+    return dist;
+}
+
+Console.WriteLine($"Расстояние между точками: {KoordDist(x1, y1, z1, x2, y2, z2)}");

39c5b2db400f514a25240f39e9c61201aeddab8f
remishevskaya <heyjude945@gmail.com>
2023-03-16 23:48:21 +0300

    Сделала 23 задание

---
+++ Seminar3/Task23/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int num)
+{
+    for (int i = 1; i <= num; i++)
+        Console.WriteLine(i * i * i);
+}
+
+Cube(num);

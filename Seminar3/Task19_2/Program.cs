a19c7589655cab66d41a9c257d2998a705e26b72
remishevskaya <heyjude945@gmail.com>
2023-03-17 21:02:07 +0300

    Сделала задание 19_2

---
+++ Seminar3/Task19_2/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void NumPal(int num)
+{
+    int count = 1;
+    int num2 = num;
+    int check = 0;
+    while (num / 10 != 0)
+    {
+        num = num / 10;
+        count++;
+    }
+
+    int[] number1 = new int[count];
+
+    for (int i = 0; i < count; i++)
+    {
+        number1[i] = num2 % 10;
+        num2 = num2 / 10;
+    }
+››
+    for (int i = 0; i <= number1.Length / 2; i++)
+    {
+        if (number1[i] == number1[(number1.Length - 1) - i])
+        {
+            check = check + 1;
+        }
+    }
+    if (check == (count / 2) + 1)
+    {
+        Console.Write("Число является палиндромом");
+    }
+    else
+    {
+        Console.Write("Число не является палиндромом");
+    }
+}
+
+NumPal(num);

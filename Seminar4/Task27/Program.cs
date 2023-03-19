dc78abcf4c12956b17384eae2ca3048c8aacaf12
remishevskaya <heyjude945@gmail.com>
2023-03-19 22:36:38 +0300

    Сделала задание 27_2

---
+++ Seminar4/Task27/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Write("Введите число: ");
+double num = Convert.ToDouble(Console.ReadLine());
+
+decimal num2 = Convert.ToDecimal(num);
+
+
+decimal sum = 0;
+
+decimal SumNum(decimal num2)
+{
+    while (num2 >= 1)
+    {
+        sum = sum + (num2 % 10);
+        num2 = Math.Truncate(num2 / 10);
+
+    }
+
+
+    if (sum - Math.Truncate(sum) > 0)
+    {
+        num2 = sum - Math.Truncate(sum);
+        sum = Math.Truncate(sum);
+
+        while (num2 > 0)
+        {
+            sum = sum + Math.Truncate(num2 * 10 % 10);
+            num2 = num2 * 10 - Math.Truncate(num2 * 10);
+        }
+    }
+    return sum;
+}
+
+
+Console.WriteLine(SumNum(num2));
+
+

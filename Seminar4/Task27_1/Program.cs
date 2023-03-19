5f5b9fbef4a1a802897130f2e8564744b9e67d72
remishevskaya <heyjude945@gmail.com>
2023-03-19 18:29:01 +0300

    Сделала задание 27_1

---
+++ Seminar4/Task27_1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string SumNum(int num)
+{
+    int sum = 0;
+    int num2 = num;
+    string message = string.Empty;
+
+    while (num != 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+
+    }
+
+    message = $"Сумма цифр числа {num2} равна {sum}";
+    return message;
+}
+
+Console.Write(SumNum(num));

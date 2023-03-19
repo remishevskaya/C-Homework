c3bd58a692f1136cff6569414a82f2a6cb90f8b9
remishevskaya <heyjude945@gmail.com>
2023-03-19 16:33:46 +0300

    Сделала задачу 25

---
+++ Seminar4/Task25/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите степень: ");
+int deg = Convert.ToInt32(Console.ReadLine());
+
+int Mult(int num, int deg)
+{
+    int result = 1;
+
+    for (int i = 1; i <= deg; i++)
+    {
+        result = result * num;
+    }
+    
+    return result;
+}
+
+Console.Write(Mult(num, deg));

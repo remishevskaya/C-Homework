dcd57d8da0bd74c65538d235949b38edfaaf5608
remishevskaya <heyjude945@gmail.com>
2023-03-30 21:39:06 +0300

    Исправила задание 34

---
+++ Seminar5/Task34/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] RandArray = new int[new Random().Next(1, 20)];
+int even = 0;
+
+
+string ElemEven(int[] array)
+{
+    string message = string.Empty;
+
+    foreach (int i in RandArray)
+    {
+        if (i % 2 == 0) even = even + 1;
+    }
+
+    message = $"Количество четных элементов: {even}";
+
+    return message;
+}
+
+Console.WriteLine(ElemEven(RandArray));

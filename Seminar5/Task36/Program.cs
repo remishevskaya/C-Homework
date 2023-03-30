01867281f9705c22d36f92ce10779dec4beb69c0
remishevskaya <heyjude945@gmail.com>
2023-03-30 21:32:05 +0300

    Сделала задание 36

---
+++ Seminar5/Task36/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] RandArray = new int[4];
+
+string ArraySum(int [] array)
+{
+    int sum = 0;
+    string message = string.Empty;
+
+    for (int i = 0; i < RandArray.Length; i++) RandArray[i] = new Random().Next(-99, 101);
+
+    foreach (int i in RandArray)
+    {
+        if (Array.IndexOf(RandArray, i) % 2 != 0) sum = sum + i;
+    }
+    message = $"Сумма чисел с нечетным индексом: {sum}";
+    return message;
+}
+
+
+Console.WriteLine(ArraySum(RandArray));

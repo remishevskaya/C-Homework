caa75169b73df11f63b8d090b8fff059eb478887
remishevskaya <heyjude945@gmail.com>
2023-03-30 21:51:05 +0300

    Сделала задание 38

---
+++ Seminar5/Task38/Program.cs
@@ -0,0 +1,16 @@
+﻿double[] RandArray = new double[10];
+double dif = 0;
+
+double DifArray(double[] array)
+{
+    for (int i = 0; i < RandArray.Count(); i++)
+    {
+        RandArray[i] = Math.Round(new Random().NextDouble() * 100, 2);
+    }
+
+    dif = RandArray.Max() - RandArray.Min();
+
+    return dif;
+}
+
+Console.WriteLine(DifArray(RandArray));

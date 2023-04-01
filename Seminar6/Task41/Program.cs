4997bf3d92b6dfee7e38ed96a25ffb58b6f32dc8
remishevskaya <heyjude945@gmail.com>
2023-04-01 11:23:45 +0300

    Сделала задание 46

---
+++ Seminar6/Task41/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Введите размерность массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] NumArray = new int[size];
+
+string SumPos(int[] array)
+{
+    string message = string.Empty;
+    int sumPos = 0;
+
+    for (int i = 0; i < NumArray.Length; i++)
+    {
+        Console.Write($"Введите {i + 1} число массива: ");
+        NumArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    foreach (int i in NumArray)
+    {
+        Console.Write($"{i} ");
+        if (i > 0) sumPos++;
+    }
+
+    message = $"Количество положительных чисел в массиве: {sumPos}";
+
+    return message;
+}
+
+Console.WriteLine($"\n{SumPos(NumArray)}");

252dba91aec0b893722766a9fe7df9aa1b5c657a
remishevskaya <heyjude945@gmail.com>
2023-04-02 19:17:48 +0300

    Сделала задание 47

---
+++ Seminar7/Task47/Program.cs
@@ -0,0 +1,22 @@
+﻿double[,] Array = new double[3, 4];
+
+
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+        for (int j = 0; j < Array.GetLength(1); j++)
+            Array[i, j] = Math.Round(new Random().NextDouble() * new Random().Next(-100, 101), 1);
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+            Console.Write($"{Array[i, j],4} \t");
+        Console.WriteLine("");
+    }
+}
+
+FillArray(Array);
+PrintArray(Array);

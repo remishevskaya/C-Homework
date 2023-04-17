be273f10aeba359a731f74ae108d75ce68710d70
remishevskaya <heyjude945@gmail.com>
2023-04-17 22:53:35 +0300

    Сделала задание 66

---
+++ Seminar9/Task66/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите начальное значение ряда: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите начальное значение ряда: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int RecSum(int m, int n)
+{
+    if (n == m) return m;
+    return n + RecSum(m, n - 1);
+}
+
+Console.WriteLine(RecSum(m, n));

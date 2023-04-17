b9ba38917bb20be54201d0b9e4bab732d0de4aba
remishevskaya <heyjude945@gmail.com>
2023-04-17 22:41:45 +0300

    Сделала задание 64

---
+++ Seminar9/Task64/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите начальное значение ряда: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string RecPrint(int num)
+{
+    if (num == 1) return Convert.ToString(num);
+    return num + " " + RecPrint(num - 1);
+}
+
+Console.WriteLine(RecPrint(num));

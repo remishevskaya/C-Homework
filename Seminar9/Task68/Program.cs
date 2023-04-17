eb94bb56b9c317e575e145b10cfd4e1f1aed4ddc
remishevskaya <heyjude945@gmail.com>
2023-04-17 23:06:37 +0300

    Сделала задание 68

---
+++ Seminar9/Task68/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите начальное значение ряда: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите начальное значение ряда: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int AkkFunction(int m, int n)
+{
+    if(m == 0) return n + 1;
+    else if (m > 0 & n == 0) return AkkFunction(m - 1, 1);
+    else return AkkFunction(m - 1, AkkFunction(m, n - 1));
+}
+
+Console.WriteLine(AkkFunction(m, n));
